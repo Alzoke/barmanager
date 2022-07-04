f8fa1fab83d35e378a310b3e801cf3c2ba1c30de
Alzoke <barkanalexi@gmail.com>
2022-07-04 21:46:20 +0300

    Controllers update.

--- src/main/java/com/example/barmanager/backend/controllers/InventoryController.java
+++ src/main/java/com/example/barmanager/backend/controllers/InventoryController.java
@@ -4,36 +4,46 @@ import com.example.barmanager.backend.assemblers.BarDrinkAssembler;
 import com.example.barmanager.backend.exceptions.DrinkNotFoundException;
 import com.example.barmanager.backend.models.BarDrink;
 import com.example.barmanager.backend.repositories.InventoryRepo;
-import com.example.barmanager.backend.service.CocktailDBAPIService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @RestController
 public class InventoryController {
 
-    private final InventoryRepo barDrinkRepo;
+    private final InventoryRepo inventoryRepo;
     private final BarDrinkAssembler barDrinkAssembler;
+    private final Logger logger;
 
 
-    public InventoryController(InventoryRepo barDrinkRepo, BarDrinkAssembler barDrinkAssembler, CocktailDBAPIService cocktailAPIService) {
-        this.barDrinkRepo = barDrinkRepo;
+    public InventoryController(InventoryRepo barDrinkRepo, BarDrinkAssembler barDrinkAssembler) {
+        this.inventoryRepo = barDrinkRepo;
         this.barDrinkAssembler = barDrinkAssembler;
+        this.logger = LoggerFactory.getLogger(InventoryController.class);
     }
 
     @PostMapping("/inventory/")
-    public ResponseEntity<EntityModel<BarDrink>> createBarDrink(@RequestBody BarDrink barDrink){
-        return ResponseEntity.ok(barDrinkAssembler.toModel(barDrinkRepo.save(barDrink)));
+    public ResponseEntity<?> createBarDrink(@RequestBody BarDrink barDrink){
+        EntityModel<BarDrink> drinkEntity = barDrinkAssembler.toModel(inventoryRepo.save(barDrink));
+        try {
+            return ResponseEntity.created(new URI(
+                    drinkEntity.getRequiredLink(IanaLinkRelations.SELF).getHref())).body(drinkEntity);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().body("Error while creating and saving the drink");
+        }
     }
 
     @GetMapping("/inventory/{id}")
     public ResponseEntity<?> getBarDrink(@PathVariable String id) {
-       return barDrinkRepo.findById(id)
+       return inventoryRepo.findById(id)
                .map(barDrinkAssembler::toModel)
                .map(ResponseEntity::ok)
                .orElseThrow(()-> new DrinkNotFoundException(id));
@@ -41,26 +51,41 @@ public class InventoryController {
 
     @GetMapping("/inventory")
     public ResponseEntity<CollectionModel<EntityModel<BarDrink>>> getAllDrinks() {
-        return ResponseEntity.ok(barDrinkAssembler.toCollectionModel(barDrinkRepo.findAll()));
+        return ResponseEntity.ok(barDrinkAssembler.toCollectionModel(inventoryRepo.findAll()));
 
     }
 
-    //@DeleteMapping("/inventory")
+    /**
+     * Check if the DB contains drink with the corresponding id , if not return DrinkNotFoundException
+     * Otherwise delete the drink and return it as response.
+     * @param id of the drink to be deleted
+     * @return ResponseEntity.ok(BarDrink Entity of the deleted drink) or DrinkNotFoundException
+     */
+    @DeleteMapping("/inventory/{id}")
+    public ResponseEntity<EntityModel<BarDrink>> deleteDrink(@PathVariable String id){
+        EntityModel<BarDrink> drinkEntity = inventoryRepo.findById(id)
+                .map(barDrinkAssembler::toModel).orElseThrow(()-> new DrinkNotFoundException(id));
+        inventoryRepo.deleteById(id);
+        return ResponseEntity.ok(drinkEntity);
+    }
 
-//    @GetMapping("/inventory/categories")
-//    public ResponseEntity<List<DrinkCategories>> showDrinksCategories()
-//    {
-//        CompletableFuture<List<DrinkCategories>> categoriesFuture = cocktailAPIService.fetchDrinkCategories();
-//        List<DrinkCategories> drinkCategories = null;
-//        try
-//        {
-//            return ResponseEntity.ok(cocktailAPIService.fetchDrinkCategories().get());
-//        }
-//        catch (InterruptedException | ExecutionException e)
-//        {
-//            e.printStackTrace();
-//        }
-//        System.out.println(drinkCategories);
-//        return null;
-//    }
+    @PutMapping("/inventory/{id}")
+    public ResponseEntity<EntityModel<BarDrink>> updateDrink(@PathVariable String id, @RequestBody BarDrink body){
+       return ResponseEntity.ok(barDrinkAssembler.toModel(inventoryRepo.updateDrink(id, body)));
+    }
+
+    @GetMapping("/inventory/filterByCategory")
+    public ResponseEntity<CollectionModel<EntityModel<BarDrink>>> getAllDrinksFilteredByCategory(@RequestParam String category){
+        return ResponseEntity.ok(barDrinkAssembler.toCollectionModel(inventoryRepo.findAllByCategory(category)));
+    }
+
+    @GetMapping("/inventory/filterByIngredient")
+    public ResponseEntity<CollectionModel<EntityModel<BarDrink>>> getAllDrinksFilteredByIngredient(@RequestParam String ingredient){
+        return ResponseEntity.ok(barDrinkAssembler.toCollectionModel(inventoryRepo.findAllByIngredientsContains(ingredient)));
+    }
+
+    @GetMapping("/inventory/filterByPriceRange")
+    public ResponseEntity<CollectionModel<EntityModel<BarDrink>>> getDrinksFilteredByPriceRange(@RequestParam Double min, Double max){
+        return ResponseEntity.ok(barDrinkAssembler.toCollectionModel(inventoryRepo.findByPriceBetween(min,max)));
+    }
 }
--- src/main/java/com/example/barmanager/backend/repositories/InventoryRepo.java
+++ src/main/java/com/example/barmanager/backend/repositories/InventoryRepo.java
@@ -3,5 +3,23 @@ package com.example.barmanager.backend.repositories;
 import com.example.barmanager.backend.models.BarDrink;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface InventoryRepo extends MongoRepository<BarDrink, String> {
+    public List<BarDrink> findAllByCategory(String category);
+    public List<BarDrink> findAllByIngredientsContains(String ingredient);
+    public default BarDrink updateDrink(String id, BarDrink newDrink){
+        BarDrink oldDrink = findById(id).orElse(newDrink);
+
+        //new BarDrink was created because the DB didn't have drink with the corresponding id.
+        if (oldDrink.equals(newDrink)){
+            oldDrink.setId(id);
+            save(oldDrink);
+            return oldDrink;
+        }
+        UpdateUtil.copyNullProperties(newDrink, oldDrink);
+        save(oldDrink);
+        return oldDrink;
+    }
+    public List<BarDrink> findByPriceBetween(Double lower, Double upper);
 }
